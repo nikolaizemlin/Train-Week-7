86b793aa3b76131fab24893eaee1a6bc88bad00f
Nick Hosley <nhosley1976@gmail.com>
2018-06-14 09:24:33 -0700

    added clock

--- assets/js/train.js
+++ assets/js/train.js
@@ -1,11 +1,54 @@
 // 
-var clock = new Vue({
-    el: '#clock',
-    data: {
-        time: '',
-        date: ''
-    }
-});
+var $dOut = $('#date'),
+    $hOut = $('#hours'),
+    $mOut = $('#minutes'),
+    $sOut = $('#seconds'),
+    $ampmOut = $('#ampm');
+var months = [
+  'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+var days = [
+  'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+];
+
+function update(){
+  var date = new Date();
+  
+  var ampm = date.getHours() < 12
+             ? 'AM'
+             : 'PM';
+  
+  var hours = date.getHours() == 0
+              ? 12
+              : date.getHours() > 12
+                ? date.getHours() - 12
+                : date.getHours();
+  
+  var minutes = date.getMinutes() < 10 
+                ? '0' + date.getMinutes() 
+                : date.getMinutes();
+  
+  var seconds = date.getSeconds() < 10 
+                ? '0' + date.getSeconds() 
+                : date.getSeconds();
+  
+  var dayOfWeek = days[date.getDay()];
+  var month = months[date.getMonth()];
+  var day = date.getDate();
+  var year = date.getFullYear();
+  
+  var dateString = dayOfWeek + ', ' + month + ' ' + day + ', ' + year;
+  
+  $dOut.text(dateString);
+  $hOut.text(hours);
+  $mOut.text(minutes);
+  $sOut.text(seconds);
+  $ampmOut.text(ampm);
+} 
+
+update();
+window.setInterval(update, 1000);
 
 var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 var timerID = setInterval(updateTime, 1000);
@@ -95,8 +138,6 @@ console.log(tMinutes);
 
 // add train data to html table
 $("#Schedule > tbody").append("<tr><td>" + tTrain + "</td><td>" + tDestination + "</td><td>" + tFrequency + " min</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
-
-
 
 });
 
